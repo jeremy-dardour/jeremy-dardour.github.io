0c67e3d0dc2a1db6a535eaf08eecb2134cf1f2db
jeremD <jerem.dardour@gmail.com>
2018-08-16 09:30:21 +0200

    INIT

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { PropTypes } from 'react';
+import Header from './Sections/Header'
+import AboutMe from './Sections/AboutMe'
+import Contact from './Sections/Contact'
+import Experiences from './Sections/Experiences'
+import Footer from './Sections/Footer'
+import Talks from './Sections/Talks'
+
+let App = ({ profile ,strings, lang, toShow, sortedTags, onLangChange, onClickTag, showAllTags, toggleShowAllTags }) => {
+  return (
+	  <div>
+      <Header strings={strings} lang={lang} onLangChange={onLangChange} />
+
+			<div id="main">
+        <AboutMe strings={strings} profile={profile} />
+
+        <Talks strings={strings} profile={profile} />
+        {/* <Articles strings={strings} profile={profile} /> */}
+
+        <Experiences profile={profile} strings={strings} toShow={toShow} sortedTags={sortedTags} onClickTag={onClickTag}
+                      toggleShowAllTags={toggleShowAllTags} showAllTags={showAllTags} />
+
+        <Contact strings={strings} profile={profile} />
+			</div>
+
+      <Footer strings={strings} profile={profile}/>
+	  </div>
+  );
+}
+
+App.propTypes = {
+  profile: PropTypes.object.isRequired,
+  strings: PropTypes.object.isRequired,
+  lang: PropTypes.string.isRequired,
+  toShow: PropTypes.array.isRequired,
+  sortedTags: PropTypes.array.isRequired,
+  onLangChange: PropTypes.func.isRequired,
+  onClickTag: PropTypes.func.isRequired,
+  showAllTags: PropTypes.bool.isRequired,
+  toggleShowAllTags: PropTypes.func.isRequired
+}
+
+export default App;
---
+++ src/Sections/Talks.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+let Player = ({ talk_link ,iframe_url}) => {
+  if (iframe_url) return <iframe src={iframe_url} width="640" height="360" frameBorder="0" allowFullScreen></iframe>;
+
+  if (talk_link && talk_link.indexOf('youtube') > 0) return <iframe src={iframe_url} width="640" height="360" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>;
+
+  return <div></div>;
+}
+
+let Talk = ({ talk_label, talk_link, iframe_url }) => (
+  <li>
+    <Player talk_link={talk_link} iframe_url={iframe_url} />
+    <br />
+    <a href={talk_link} target="_blank">{talk_label}</a>
+  </li>
+);
+
+let Talks = ({strings, profile}) => {
+  const talks = profile.last_talks;
+  console.log(talks);
+
+  return (
+    <section id="talks">
+    <header className="major">
+      <h2>{strings.talks_title}</h2>
+    </header>
+      <ul className="alt">
+        {talks && talks.map(talk => <Talk key={talk.id} {...talk} />)}
+      </ul>
+    </section>
+  )
+}
+
+export default Talks;
